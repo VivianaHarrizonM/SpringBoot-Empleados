a0492406221d25304982ba431415a7b24ff649f5
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-04-28 18:20:57 -0600

    Project ready to Render

--- src/main/java/vhm/empleados/EmpleadosApplication.java
+++ src/main/java/vhm/empleados/EmpleadosApplication.java
@@ -2,9 +2,16 @@ package vhm.empleados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmpleadosApplication {
+public class EmpleadosApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EmpleadosApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleadosApplication.class, args);
